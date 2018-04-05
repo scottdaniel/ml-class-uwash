b072887913c878cd7aac07fce374d3e0068ed2a7
Scott Daniel <scottdaniel25@gmail.com>
2018-04-05 10:52:54 -0700

    learn something new every day

--- prog.py
+++ prog.py
@@ -1,5 +1,7 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("echo", help="echo the string you use here")
+parser.add_argument("--verbosity", help="increase output verbosity")
 args = parser.parse_args()
-print(args.echo)
+if args.verbosity:
+    print("verbosity turned on")
+
